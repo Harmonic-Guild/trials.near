df053db954398ce683e133c5c5c9b11516179025
jaswinder6991 <jassmultani2@gmail.com>
2024-06-17 18:46:58 +0530

    fix: fixed footer

--- apps/trial-accounts/widget/navigation/Footer.jsx
+++ apps/trial-accounts/widget/navigation/Footer.jsx
@@ -1,7 +1,3 @@
-const { Button } = VM.require("abdullahi3000.near/widget/components") || {
-  Button: () => <></>,
-};
-
 const logoLink =
   "https://res.cloudinary.com/dtt0cjt51/image/upload/v1711122280/cafg8h33bpq15uugd8ta.png";
 
@@ -28,7 +24,7 @@ const Card = styled.div`
   width: 100%;
 
   border-radius: 16px;
-  border: 1px solid var(--Gradient-1, #2287C2);
+  border: 1px solid var(--Gradient-1, #2287c2);
   background-color: #0b0c14;
   box-shadow: 4px 24px 48px 0px rgba(249, 225, 122, 0.05);
 
@@ -119,13 +115,7 @@ const LinksContainer = styled.div`
 `;
 
 const XIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
     <path
       d="M8 2.75H1L9.26086 13.7645L1.44995 22.7499H4.09998L10.4883 15.401L16 22.75H23L14.3917 11.2723L21.8001 2.75H19.1501L13.1643 9.63578L8 2.75ZM17 20.75L5 4.75H7L19 20.75H17Z"
       fill="black"
@@ -133,44 +123,28 @@ const XIcon = () => (
   </svg>
 );
 
-  return (
-    <Container className="container-xl">
-      {/* {!noBanner && (
-        <Card>
-          <div className="d-flex flex-column align-items-center">
-            <img src={logoLink} />
-            <h3>
-              Let{"’"}s Make <span>Waves!</span>
-            </h3>
-          </div>
-          <a href="https://near.social/abdullahi3000.near/widget/app?page=joinUs">
-            <Button variant="primary" noLink={true}>
-              Join Now
-            </Button>
-          </a>
-        </Card>
-      )} */}
-      <FooterContainer>
-        <LinksContainer>
-          <a
-            href="https://twitter.com/harmonicguild"
-            className="d-flex align-items-center"
-            target="_blank"
-          >
-            <XIcon />
-          </a>
-          <a href="https://harmonicguild.io" target="_blank">
-            <i className="bi bi-globe"></i>
-          </a>
-          <a href="https://t.me/harmoniccommunity" target="_blank">
-            <i className="bi bi-telegram"></i>
-          </a>
-          <a href="https://github.com/Harmonic-Guild" target="_blank">
-            <i className="bi bi-github"></i>
-          </a>
-        </LinksContainer>
-        <p>©{new Date().getFullYear()} Harmionic Guild- All rights reserved</p>
-      </FooterContainer>
-    </Container>
-  );
-
+return (
+  <Container className="container-xl">
+    <FooterContainer>
+      <LinksContainer>
+        <a
+          href="https://twitter.com/harmonicguild"
+          className="d-flex align-items-center"
+          target="_blank"
+        >
+          <XIcon />
+        </a>
+        <a href="https://harmonicguild.io" target="_blank">
+          <i className="bi bi-globe"></i>
+        </a>
+        <a href="https://t.me/harmoniccommunity" target="_blank">
+          <i className="bi bi-telegram"></i>
+        </a>
+        <a href="https://github.com/Harmonic-Guild" target="_blank">
+          <i className="bi bi-github"></i>
+        </a>
+      </LinksContainer>
+      <p>©{new Date().getFullYear()} Harmionic Guild- All rights reserved</p>
+    </FooterContainer>
+  </Container>
+);
